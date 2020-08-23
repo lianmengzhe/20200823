b57f5e3fba8aeb8778175082ba58299b0cae5b14
lyumay <1241015526@qq.com>
2020-08-23 20:02:49 +0800

    胡帅 MVC作业20200823

---
+++ application/config.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    "database"=>array(
+        "host"=>"localhost",
+        "username"=>"root",
+        "password"=>"983626hu",
+        "dbname"=>"wui2006",
+        "port"=>"3306",
+    ),
+    "smarty"=>array(
+        "templateDir"=>TPL_PATH,
+        "compileDir"=>COMPILE_PATH,
+        "cacheDir"=>CACHE_PATH,
+    ),
+
+
+);
